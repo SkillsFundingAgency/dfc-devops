33d2dc3d1812311a000ab327aec8ffc554e61f7c
Richard Lee <ric@dicci.co.uk>
2018-11-22 21:27:07 +0000

    Mock attempt back to start

--- Tests/A001.PSScripts.Test-ARMTemplate.Tests.ps1
+++ Tests/A001.PSScripts.Test-ARMTemplate.Tests.ps1
@@ -1,10 +1,10 @@
 Push-Location -Path $PSScriptRoot\..\PSScripts\
 
-Mock Test-AzureRmResourceGroupDeployment
 
 Describe "Test-ARMTemplate acceptance tests" -Tag "Acceptance" {
 
     It "Should call Test-AzureRmResourceGroupDeployment" {
+        Mock Test-AzureRmResourceGroupDeployment {}
         .\Test-ARMTemplate -TemplateFile template.json -ParameterFile parameters.json
         Assert-MockCalled Test-AzureRmResourceGroupDeployment  #-Exactly 1
     }
