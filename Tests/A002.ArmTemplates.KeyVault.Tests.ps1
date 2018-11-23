c0577b41796822740dbfef3f99bd0443bc19eb71
Richard Lee <ric@dicci.co.uk>
2018-11-23 12:07:15 +0000

    Wrong calling of cmdlet

--- Tests/A002.ArmTemplates.KeyVault.Tests.ps1
+++ Tests/A002.ArmTemplates.KeyVault.Tests.ps1
@@ -4,19 +4,21 @@ $TemplateFile = "$PSScriptRoot\..\ArmTemplates\keyvault.json"
 
 Describe "Key Vault Deployment Tests" -Tag "Acceptance" {
   
-    Context "When KeyVault deployed with just key vault name" {
-      $TemplateParameters = @{ keyVaultName = "dfc-foo-bar-kv" }
-      $TestTemplateParams = @{
-        ResourceGroupName       = $ResourceGroupName
-        TemplateFile            = $TemplateFile
-        TemplateParameterObject = $TemplateParameters
-      }
-
-      $output = Test-AzureRmResourceGroupDeployment $TestTemplateParams
-  
-      It "Should be deployed successfully" {
-        $output | Should -Be $null
-      }
-
+  Context "When KeyVault deployed with just key vault name" {
+    $TemplateParameters = @{
+      keyVaultName = "dfc-foo-bar-kv"
     }
+    $TestTemplateParams = @{
+      ResourceGroupName       = $ResourceGroupName
+      TemplateFile            = $TemplateFile
+      TemplateParameterObject = $TemplateParameters
+    }
+
+    $output = Test-AzureRmResourceGroupDeployment @TestTemplateParams
+  
+    It "Should be deployed successfully" {
+      $output | Should -Be $null
+    }
+
+  }
 }
