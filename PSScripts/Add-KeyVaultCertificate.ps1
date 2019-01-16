18aea125267019859c0080c311274de20acc9d03
Richard Lee <ric@dicci.co.uk>
2019-01-16 15:31:07 +0000

    Style changes

--- PSScripts/Add-KeyVaultCertificate.ps1
+++ PSScripts/Add-KeyVaultCertificate.ps1
@@ -5,35 +5,35 @@ Imports pfx certificate to a keyvault for use in Azure
 .DESCRIPTION
 Imports pfx certificate to a keyvault for use in Azure
 
-.PARAMETER keyVaultName
+.PARAMETER KeyVaultName
 Keyvault to add the secret to
 
-.PARAMETER secretName
+.PARAMETER SecretName
 Name of the 
 
-.PARAMETER pfxFilePath
+.PARAMETER PfxFilePath
  Full path to the pfx file including file name
 
-.PARAMETER pfxPassword
+.PARAMETER PfxPassword
 Password for the pfx file
 
 .EXAMPLE
-Add-KeyVaultCertificate -keyVaultName dfc-foo-kv -secretName mycert -pfxFilePath C:\path\to\cert.pfx -pfxPassword myPa$$w0rd
+Add-KeyVaultCertificate -KeyVaultName dfc-foo-kv -SecretName mycert -PfxFilePath C:\path\to\cert.pfx -PfxPassword myPa$$w0rd
 
 #>
 param(
     [Parameter(Mandatory=$true)]
-    [string] $keyVaultName,
+    [string] $KeyVaultName,
     [Parameter(Mandatory=$true)]
-    [string] $secretName,
+    [string] $SecretName,
     [Parameter(Mandatory=$true)]
-    [string] $pfxFilePath,
+    [string] $PfxFilePath,
     [Parameter(Mandatory=$true)]
-    [string] $pfxPassword
+    [string] $PfxPassword
 )
   
 $collection = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2Collection
-$collection.Import($pfxFilePath, $pfxPassword, [System.Security.Cryptography.X509Certificates.X509KeyStorageFlags]::Exportable)
+$collection.Import($PfxFilePath, $PfxPassword, [System.Security.Cryptography.X509Certificates.X509KeyStorageFlags]::Exportable)
     
 # create secret variables from cert
 $expdate = $collection.NotAfter | Sort-Object | Select-Object -First 1 # earliest expiring cert in pfx
@@ -41,5 +41,5 @@ $clearBytes = $collection.Export([System.Security.Cryptography.X509Certificates.
 $fileContentEncoded = [System.Convert]::ToBase64String($clearBytes)
 $secret = ConvertTo-SecureString -String $fileContentEncoded -AsPlainText -Force
 $secretContentType = 'application/x-pkcs12'
-$secretName = $secretName.Replace('.' , '-')
-Set-AzureKeyVaultSecret -VaultName $keyVaultName -Name $secretName -SecretValue $Secret -ContentType $secretContentType -Expires $expdate
+$SecretName = $SecretName.Replace('.' , '-')
+Set-AzureKeyVaultSecret -VaultName $KeyVaultName -Name $SecretName -SecretValue $Secret -ContentType $secretContentType -Expires $expdate
--- Tests/U002.PSScripts.Add-KeyVaultCertificate.Tests.ps1
+++ Tests/U002.PSScripts.Add-KeyVaultCertificate.Tests.ps1
@@ -10,7 +10,7 @@ Describe "Add-KeyVaultCertificate unit tests" -Tag "Unit" {
         $certdate = Get-Date "25 January 2020 11:51:04"
 
         .\Add-KeyVaultCertificate -keyVaultName $kvname -secretName $secname -pfxFilePath "$PSScriptRoot\testcert.pfx" -pfxPassword 'myPa$$w0rd'
-        #Assert-MockCalled Set-AzureKeyVaultSecret -Exactly 1 -Scope It
+
         Assert-MockCalled Set-AzureKeyVaultSecret -Exactly 1 -ParameterFilter { $VaultName -eq $kvname -and $Name -eq $secname -and $ContentType -eq 'application/x-pkcs12' -and $Expires -eq $certdate } -Scope It
     }
 
