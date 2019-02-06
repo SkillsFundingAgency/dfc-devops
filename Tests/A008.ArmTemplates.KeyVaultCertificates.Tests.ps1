1564836c2814a0f25832dea191caa690050588ab
NickGraham101 <nick.graham101@googlemail.com>
2019-02-06 14:23:02 +0000

    Fixed paths on KeyVault tests

--- Tests/A008.ArmTemplates.KeyVaultCertificates.Tests.ps1
+++ Tests/A008.ArmTemplates.KeyVaultCertificates.Tests.ps1
@@ -1,6 +1,6 @@
 # common variables
 $ResourceGroupName = "dfc-test-template-rg"
-$TemplateFile = "$PSScriptRoot\..\ArmTemplates\keyvault-certificates.json"
+$TemplateFile = "$PSScriptRoot\..\ArmTemplates\KeyVault\keyvault-certificates.json"
 
 Describe "Key Vault Secrets Deployment Tests" -Tag "Acceptance" {
   
