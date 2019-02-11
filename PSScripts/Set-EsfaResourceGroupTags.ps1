3825ca99fa6b385c935ce59c2ddb53f855afca62
Nick Graham <34914709+NickGraham101@users.noreply.github.com>
2019-02-11 11:35:50 +0000

    Added script and test to set resource group tags (#23)
    * Added script and test to set resource group tags
    
    * Added missing help text
    
    * Added missing param descriptions

---
+++ PSScripts/Set-EsfaResourceGroupTags.ps1
@@ -0,0 +1,101 @@
+<#
+.SYNOPSIS
+Sets standard tags on a resource group required by the ESFA.
+
+.DESCRIPTION
+Checks if a resource group exists, if it doesn't creates with specified tags.  If it does validates that the tags are match those specified in the parameters and updates them if necessary.
+
+.PARAMETER ResourceGroupName
+Name of the resource group to be created and \ or have tags applied
+
+.PARAMETER Location
+[Optional]Location of the resource group, defaults to West Europe
+
+.PARAMETER Environment
+Name of the environment, select from a valid ESFA environment name tag: Production, PreProduction or Dev/Test
+
+.PARAMETER ParentBusiness
+Name of the business to which the resources belong, select from either National Careers Service or National Careers Service (PP)
+
+.PARAMETER ServiceOffering
+Name of the service offering to which the resources belong, select from Course Directory, Course Directory (PP), Data Sharing Service, Data Sharing Service (PP), Digital First Career Service (DFCS) Website, Digital First Career Service (DFCS) Website (PP), NCS Website or NCS Website (PP)
+
+.EXAMPLE
+Set-EsfaResourceGroupTags -ResourceGroupName "dfc-dev-foobar-rg" -Environment "Dev/Test" -ParentBusiness "National Careers Service" -ServiceOffering "Course Directory"
+#>
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$ResourceGroupName,
+    [Parameter(Mandatory=$false)]
+    [string]$Location = "West Europe",
+    [Parameter(Mandatory=$true)]
+    [ValidateSet("Production", "PreProduction", "Dev/Test")]
+    [string]$Environment,
+    [Parameter(Mandatory=$true)]
+    [ValidateSet("National Careers Service", "National Careers Service (PP)")]
+    [string]$ParentBusiness,
+    [Parameter(Mandatory=$true)]
+    [ValidateSet("Course Directory", "Course Directory (PP)", "Data Sharing Service", "Data Sharing Service (PP)", "Digital First Career Service (DFCS) Website", "Digital First Career Service (DFCS) Website (PP)", "NCS Website", "NCS Website (PP)")]
+    [string]$ServiceOffering
+)
+
+$Tags = @{
+    Environment = $Environment
+    'Parent Business' = $ParentBusiness
+    'Service Offering' = $ServiceOffering
+}
+
+Write-Verbose -Message "Attempting to retrieve existing resource group $ResourceGroupName"
+$ResourceGroup = Get-AzureRmResourceGroup -Name $ResourceGroupName
+
+if(!$ResourceGroup) {
+
+    Write-Verbose -Message "Resource group $ResourceGroupName doesn't exist, creating resource group"
+    New-AzureRmResourceGroup -Name $ResourceGroupName -Location $Location -Tag $Tags
+
+}
+else {
+
+    Write-Verbose -Message "Resource group $ResourceGroupName exists, validating tags"
+    $UpdatedTags = $ResourceGroup.Tags
+    $UpdateTags = $false
+
+    foreach ($Key in ($Tags.GetEnumerator() | Select-Object -Property Key)) {
+
+        Write-Verbose "Current value of Resource Group Tag $($Key.Key) is $($ResourceGroup.Tags["$($Key.Key)"])"
+        if ($($ResourceGroup.Tags["$($Key.Key)"]) -eq $($Tags["$($Key.Key)"])) {
+
+            Write-Verbose -Message "Current value of tag ($($ResourceGroup.Tags["$($Key.Key)"])) matches parameter ($($Tags["$($Key.Key)"]))"
+
+        }
+        elseif ($null -eq $($ResourceGroup.Tags["$($Key.Key)"])){
+
+            Write-Verbose -Message ("Tag value is not set, adding tag {0} with value {1}" -f $($Key.Key), $($Tags["$($Key.Key)"]))
+            $UpdatedTags["$($Key.Key)"] = $($Tags["$($Key.Key)"])
+            $UpdateTags = $true
+
+        }
+        else {
+
+            Write-Verbose -Message ("Tag value is incorrect, setting tag {0} with value {1}" -f $($Key.Key), $($Tags["$($Key.Key)"]))
+            $UpdatedTags["$($Key.Key)"] = $($Tags["$($Key.Key)"])
+            $UpdateTags = $true
+            
+        }
+
+    }
+
+    if ($UpdateTags) {
+
+        Write-Host "Replacing existing tags:"
+        $UpdatedTags
+        Set-AzureRmResourceGroup -Name $ResourceGroup.ResourceGroupName -Tag $UpdatedTags
+
+    }
+
+
+}
+
+
+
