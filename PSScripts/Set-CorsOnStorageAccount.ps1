64cb9ac83d3d02a645560e5b8a51ab2c80db3426
Richard Lee <ric@dicci.co.uk>
2019-02-27 17:13:40 +0000

    Ädded Set-CorsOnStorageAccount.ps1

---
+++ PSScripts/Set-CorsOnStorageAccount.ps1
@@ -0,0 +1,69 @@
+<#
+
+.SYNOPSIS
+Adds the CORS header to storage account responses
+
+.DESCRIPTION
+Adds the CORS header to storage account responses
+
+.PARAMETER StorageAccountName
+Name of storage account
+
+.PARAMETER StorageAccountKey
+Access key for storage account
+
+.PARAMETER AllowedOrigins
+Array of allowed origins
+
+.PARAMETER MaxAge
+Optionally set the max age to cache requests in seconds (defaults to 1 hours)
+
+.EXAMPLE
+Set-CorsOnStorageAccount -StorageAccountName dfcfoobarstr -StorageAccountKey not-a-real-key= -AllowedOrigins
+
+#>
+    
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory = $true)]
+    [string] $StorageAccountName,
+    [Parameter(Mandatory = $true)]
+    [string] $StorageAccountKey,
+    [Parameter(Mandatory = $true)]
+    [string[]] $AllowedOrigins,
+    [Parameter(Mandatory = $false)]
+    [string] $MaxAge = 3600
+)
+
+Write-Verbose "Setting Storage Context on $StorageAccountName"
+$StorageContext = New-AzureStorageContext -StorageAccountName $StorageAccountName.ToLower() -StorageAccountKey $StorageAccountKey
+$ExistingCorsRules = Get-AzureStorageCORSRule -ServiceType Blob -Context $StorageContext
+Write-Verbose "Existing: $ExistingCorsRules"
+$CORSRules = @()
+$CORSChanged = $false
+
+# Create CORS object
+foreach ($AllowedOrigin in $AllowedOrigins) {
+    Write-Verbose "Adding origin $AllowedOrigin"
+
+    $CORSRules += @{
+        AllowedHeaders  = @( "*" )
+        AllowedOrigins  = @( "$($AllowedOrigin.ToLower())" )
+        MaxAgeInSeconds = $MaxAge
+        AllowedMethods  = @( "GET", "HEAD", "OPTIONS" )
+    }
+
+    # Set CORSChanged to true if origin not already set
+    $CORSChanged = $CORSChanged -or ($AllowedOrigin.ToLower() -notin $ExistingCorsRules.AllowedOrigins)
+}
+
+if ($CORSChanged) {
+    # Rule has been added, set CORS Rules
+    try {
+        Write-Output "Setting CORS rule on $($StorageContext.BlobEndPoint)"
+        Set-AzureStorageCORSRule -ServiceType Blob -CorsRules $CORSRules -Context $StorageContext
+    }
+    catch {
+        throw "Failed to get Storage Context and set CORS settings: $_"
+    }
+}
