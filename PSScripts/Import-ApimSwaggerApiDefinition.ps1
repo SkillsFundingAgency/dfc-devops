ec19a9f55eb45d8a72a5f8a21e83b52585c3cd7a
NickGraham101 <nick.graham101@googlemail.com>
2019-02-27 14:38:57 +0000

    Added Import-ApimSwaggerApiDefinition script

---
+++ PSScripts/Import-ApimSwaggerApiDefinition.ps1
@@ -0,0 +1,77 @@
+<#
+.SYNOPSIS
+Update an APIM API with a swagger definition
+
+.PARAMETER ResourceGroupName
+The name of the resource group that contains the APIM instnace
+
+.PARAMETER InstanceName
+The name of the APIM instnace
+
+.PARAMETER ApiName
+The name of the API to update
+
+.PARAMETER SwaggerSpecificationUrl
+The full path to the swagger defintion
+
+For example:
+https://sit-manage-vacancy.apprenticeships.sfa.bis.gov.uk/swagger/docs/v1
+
+#>
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory=$true)]
+    [String]$ApimResourceGroup,
+    [Parameter(Mandatory=$true)]
+    [String]$InstanceName,
+    [Parameter(Mandatory=$true)]
+    [String]$ApiName,
+    [Parameter(Mandatory=$true)]
+    [String]$SwaggerSpecificationUrl,
+    [Parameter(Mandatory=$false, ParameterSetName="File")]
+    [Switch]$SwaggerSpecificationFile,
+	[Parameter(Mandatory=$false, ParameterSetName="File")]
+	[string]$OutputFilePath
+
+)
+
+if ($PSCmdlet.ParameterSetName -eq "File") {
+
+    $Swagger = Invoke-RestMethod -Method GET -Uri $SwaggerSpecificationUrl -UseBasicParsing
+    Write-Verbose -Message $($Swagger | ConvertTo-Json -Depth 20)
+
+    $FunctionAppName = $SwaggerSpecificationUrl.split("/")[2].split(".")[0]
+    $FileName = "$($FunctionAppName)_swagger-def_$([DateTime]::Now.ToString("yyyyMMdd-hhmmss")).json"
+    Write-Verbose -Message "Filename: $FileName"
+
+    $OutputFolder = New-Item -Path $OutputFilePath -ItemType Directory
+    $OutputFile = New-Item -Path "$($OutputFolder.FullName)\$FileName" -ItemType File
+    Write-Verbose -Message "OutputFile: $($OutputFile.FullName)"
+    Set-Content -Path $OutputFile.FullName -Value ($Swagger | ConvertTo-Json -Depth 20)
+}
+
+try {
+    # --- Build context and retrieve apiid
+    Write-Host "Building APIM context for $ApimResourceGroup\$InstanceName"
+    $Context = New-AzureRmApiManagementContext -ResourceGroupName $ApimResourceGroup -ServiceName $InstanceName
+    Write-Host "Retrieving ApiId for API $ApiName"
+    $Api = Get-AzureRmApiManagementApi -Context $Context -ApiId $ApiName
+
+    # --- Throw if Api is null
+    if (!$Api) {
+        throw "Could not retrieve Api for API $ApiName"
+    }
+
+    # --- Import swagger definition
+    
+    if($PSCmdlet.ParameterSetName -eq "Url") {
+        Write-Host "Updating API $InstanceName\$($Api.ApiId) from definition $SwaggerSpecificationUrl"
+        Import-AzureRmApiManagementApi -Context $Context -SpecificationFormat "Swagger" -SpecificationUrl $SwaggerSpecificationUrl -ApiId $($Api.ApiId) -Path $($Api.Path) -ErrorAction Stop -Verbose:$VerbosePreference
+    }
+    elseif ($PSCmdlet.ParameterSetName -eq "File") {
+        Write-Host "Updating API $InstanceName\$($Api.ApiId) from definition $($OutputFile.FullName)"
+        Import-AzureRmApiManagementApi -Context $Context -SpecificationFormat "Swagger" -SpecificationPath $($OutputFile.FullName) -ApiId $($Api.ApiId) -Path $($Api.Path) -ErrorAction Stop -Verbose:$VerbosePreference
+    }
+} catch {
+   throw $_
+}
