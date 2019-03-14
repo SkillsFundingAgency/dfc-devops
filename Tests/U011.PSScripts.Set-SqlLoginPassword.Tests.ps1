bdc776965183af1011078dd60509afb7cd78197b
Richard Lee <ric@dicci.co.uk>
2019-03-14 15:15:28 +0000

    Added check to ensure db is online before continuing

--- Tests/U011.PSScripts.Set-SqlLoginPassword.Tests.ps1
+++ Tests/U011.PSScripts.Set-SqlLoginPassword.Tests.ps1
@@ -4,7 +4,8 @@ $SQLLogin = "connection-user"
 $SQLLoginPassword = "not-a-real-password"
 
 $params = @{
-    SQLServerFqdn     = "dfc-foo-bar-sql"
+    ResourceGroupName = "dfc-foo-bar-rg"
+    SQLServerName     = "dfc-foo-bar-sql"
     SQLDatabase       = "dfc-foo-bar-db" 
     SQLAdminUsername  = "admin"
     SQLAdminPassword  = "not-a-real-password"
@@ -14,12 +15,36 @@ $params = @{
 
 # solves CommandNotFoundException
 function Invoke-Sqlcmd {}
+function Get-AzureRmSqlServer {}
+function Get-AzureRmSqlDatabase {}
 
 Describe "Set-SqlLoginPassword unit tests" -Tag "Unit" {
 
     $SQLScript = "$TestDrive\Mock.sql"
 
     Mock Invoke-Sqlcmd
+    Mock Get-AzureRmSqlServer {
+        $mock = '{ "ResourceGroupName": "dfc-foo-bar-rg", "ServerName": "dfc-foo-bar-sql", "FullyQualifiedDomainName": "dfc-foo-bar-sql.database.windows.net" }'
+        return ConvertFrom-Json $mock
+    }
+
+    # mock Get-AzureRmSqlDatabase returns offline
+    Mock Get-AzureRmSqlDatabase {
+        $offlinemock = '{ "ResourceGroupName": "dfc-foo-bar-rg", "ServerName": "dfc-foo-bar-sql", "DatabaseName": "dfc-foo-bar-db", "Status": "Offline" }'
+        return ConvertFrom-Json $offlinemock
+    }
+
+    It "Should throw an error if database is not online" {
+
+        { .\Set-SqlLoginPassword @params } | Should -Throw
+
+    }
+
+    # mock Get-AzureRmSqlDatabase returns online
+    Mock Get-AzureRmSqlDatabase {
+        $onlinemock = '{ "ResourceGroupName": "dfc-foo-bar-rg", "ServerName": "dfc-foo-bar-sql", "DatabaseName": "dfc-foo-bar-db", "Status": "Online" }'
+        return ConvertFrom-Json $onlinemock
+    }
 
     It "Should issue a single Invoke-Sqlcmd when just reseting password" {
 
@@ -35,6 +60,8 @@ Describe "Set-SqlLoginPassword unit tests" -Tag "Unit" {
 
         Mock Write-Error
 
+        $global:LoopsBeforeOnline = 0
+
         .\Set-SqlLoginPassword -UserScript $SQLScript @params
 
         Assert-MockCalled Write-Error
@@ -44,6 +71,8 @@ Describe "Set-SqlLoginPassword unit tests" -Tag "Unit" {
     It "Should run Invoke-Sqlcmd with inputfile when valid script is passed" {
 
         Set-Content -Path $SQLScript -Value "SELECT 1"
+
+        $global:LoopsBeforeOnline = 0
 
         .\Set-SqlLoginPassword @params
 
