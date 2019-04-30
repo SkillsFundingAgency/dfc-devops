4ea9f081591bae8aaf4e8dd7bc81436bb1848aa3
NickGraham101 <nick.graham101@googlemail.com>
2019-04-30 12:09:28 +0100

    Added test

--- Tests/A018.ArmTemplates.SqlDatabase.Tests.ps1
+++ Tests/A018.ArmTemplates.SqlDatabase.Tests.ps1
@@ -8,29 +8,9 @@ $TemplateParametersDefault = @{
 
 Describe "Sql Database Deployment Tests" -Tag "Acceptance" {
   
-    Context "When SQL Database is deployed with databaseName, sqlServerName and databaseTier of Basic" {
+    Context "When SQL Database is deployed with just databaseName and sqlServerName" {
 
         $TemplateParameters = $TemplateParametersDefault
-        $TemplateParameters['databaseTier'] = "Basic"
-        $TestTemplateParams = @{
-            ResourceGroupName       = $ResourceGroupName
-            TemplateFile            = $TemplateFile
-            TemplateParameterObject = $TemplateParameters
-        }
-
-        $output = Test-AzureRmResourceGroupDeployment @TestTemplateParams
-
-        It "Should be deployed successfully" {
-            $output | Should -Be $null
-        }
-
-    }
-
-    Context "When SQL Database is deployed with databaseName, sqlServerName and databaseTier of Standard and a databaseSize of 2" {
-
-        $TemplateParameters = $TemplateParametersDefault
-        $TemplateParameters['databaseTier'] = "Standard"
-        $TemplateParameters['databaseSize'] = "2"
         $TestTemplateParams = @{
             ResourceGroupName       = $ResourceGroupName
             TemplateFile            = $TemplateFile
