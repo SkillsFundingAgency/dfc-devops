d2a0e5e16ef1f4f388f9641890fd7ae2513ea513
NickGraham101 <nick.graham101@googlemail.com>
2019-05-01 10:41:36 +0100

    Added databaseSize param

--- ArmTemplates/SqlServer/sql-database.md
+++ ArmTemplates/SqlServer/sql-database.md
@@ -6,15 +6,19 @@ Creates a new SQL database.  Requires a [SQL Server ](..\sql-server.md) to alrea
 
 databaseName (required) string
 
+databaseTier (required) string
+
+Select values from "Basic", "Standard", "Premium".  This will be concatenated with the databaseSize (exception for Basic) to form the sku name.
+
 sqlServerName (required) string
 
 elasticPoolName (optional) string
 
-databaseSkuName (optional) string
+databaseSize (optional) string
+
+Select values from "1", "2", "3", "4", "6", "7", "9", "10", "11", "15".  This will be concatenated with the databaseTier (exception for Basic) to form the sku name.  Check that the combiniation of databaseTier and databaseSize are valid.
 
 databaseSizeBytes (optional) string
-
-databaseTier (optional) string
 
 dataMaskingExemptPrincipals (optional) string
 
--- Tests/A018.ArmTemplates.SqlDatabase.Tests.ps1
+++ Tests/A018.ArmTemplates.SqlDatabase.Tests.ps1
@@ -8,9 +8,29 @@ $TemplateParametersDefault = @{
 
 Describe "Sql Database Deployment Tests" -Tag "Acceptance" {
   
-    Context "When SQL Database is deployed with just databaseName and sqlServerName" {
+    Context "When SQL Database is deployed with databaseName, sqlServerName and databaseTier of Basic" {
 
         $TemplateParameters = $TemplateParametersDefault
+        $TemplateParameters['databaseTier'] = "Basic"
+        $TestTemplateParams = @{
+            ResourceGroupName       = $ResourceGroupName
+            TemplateFile            = $TemplateFile
+            TemplateParameterObject = $TemplateParameters
+        }
+
+        $output = Test-AzureRmResourceGroupDeployment @TestTemplateParams
+
+        It "Should be deployed successfully" {
+            $output | Should -Be $null
+        }
+
+    }
+
+    Context "When SQL Database is deployed with databaseName, sqlServerName and databaseTier of Standard and a databaseSize of 2" {
+
+        $TemplateParameters = $TemplateParametersDefault
+        $TemplateParameters['databaseTier'] = "Standard"
+        $TemplateParameters['databaseSize'] = "2"
         $TestTemplateParams = @{
             ResourceGroupName       = $ResourceGroupName
             TemplateFile            = $TemplateFile
