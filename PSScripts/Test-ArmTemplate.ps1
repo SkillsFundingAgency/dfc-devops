40dc94bf4f0c6e47f6c7ec27855d9389128849e8
NickGraham101 <nick.graham101@googlemail.com>
2019-05-09 14:07:17 +0100

    Added missing documentation

--- PSScripts/Test-ArmTemplate.ps1
+++ PSScripts/Test-ArmTemplate.ps1
@@ -5,12 +5,15 @@ Tests an ARM template file
 .DESCRIPTION
 Tests a ARM template file to make sure it should deploy.
 
-.PARAMETER TemplateFile
-The template file to test
-
 .PARAMETER ParameterFile
 Parameter file with manditory and optional parameters to test with.
 Normally contains dummy values unless it is a dependancy in which case a valid value is needed.
+
+.PARAMETER TemplateFile
+The template file to test
+
+.PARAMETER ResourceGroupName
+The name of the resource group to test the ARM template against.  Defaults to dfc-test-template-rg
 
 .EXAMPLE
 Test-ArmTemplate.ps1 -ParameterFile paramaters.json -TemplateFile template.json
@@ -19,8 +22,8 @@ Test-ArmTemplate.ps1 -ParameterFile paramaters.json -TemplateFile template.json
 [CmdletBinding()]
 Param(
   [string] $ParameterFile ,
-  [string] $ResourceGroupName = "dfc-test-template-rg",
-  [string] $TemplateFile
+  [string] $TemplateFile,
+  [string] $ResourceGroupName = "dfc-test-template-rg"
 )
 
 $DeploymentParameters = @{
