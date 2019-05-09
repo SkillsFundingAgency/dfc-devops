e37b1c466a6089aa3d8d905fc419a1a69bfe1bdb
Nick Graham <34914709+NickGraham101@users.noreply.github.com>
2019-05-09 14:15:55 +0100

    Ncsd 628 add resources for testing arm (#64)

---
+++ PSScripts/Test-ArmTemplate.ps1
@@ -0,0 +1,43 @@
+<#
+.SYNOPSIS
+Tests an ARM template file
+
+.DESCRIPTION
+Tests a ARM template file to make sure it should deploy.
+
+.PARAMETER ParameterFile
+Parameter file with manditory and optional parameters to test with.
+Normally contains dummy values unless it is a dependancy in which case a valid value is needed.
+
+.PARAMETER TemplateFile
+The template file to test
+
+.PARAMETER ResourceGroupName
+The name of the resource group to test the ARM template against.  Defaults to dfc-test-template-rg
+
+.EXAMPLE
+Test-ArmTemplate.ps1 -ParameterFile paramaters.json -TemplateFile template.json
+
+#>
+[CmdletBinding()]
+Param(
+  [string] $ParameterFile ,
+  [string] $TemplateFile,
+  [string] $ResourceGroupName = "dfc-test-template-rg"
+)
+
+$DeploymentParameters = @{
+  ResourceGroupName                 = $ResourceGroupName
+  TemplateFile                      = $TemplateFile
+  TemplateParameterFile             = $ParameterFile
+  Verbose = $true
+}
+
+Write-Host "- Validating template"
+if ($PSCmdlet.MyInvocation.BoundParameters["Verbose"].IsPresent) {
+
+    Write-Verbose -Message "Deployment Parameters:"
+    $DeploymentParameters
+
+}
+Test-AzureRmResourceGroupDeployment @DeploymentParameters
