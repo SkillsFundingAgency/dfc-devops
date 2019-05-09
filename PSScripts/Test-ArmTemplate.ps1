399f6f88f88281580e4c9e4494bfb68fe187a140
NickGraham101 <nick.graham101@googlemail.com>
2019-05-09 13:47:31 +0100

    Tidied up example

--- PSScripts/Test-ArmTemplate.ps1
+++ PSScripts/Test-ArmTemplate.ps1
@@ -13,7 +13,7 @@ Parameter file with manditory and optional parameters to test with.
 Normally contains dummy values unless it is a dependancy in which case a valid value is needed.
 
 .EXAMPLE
-Test-ARMTemplate.ps1 -TemplateFile template.json -ParameterFile paramaters.json
+Test-ArmTemplate.ps1 -ParameterFile paramaters.json -TemplateFile template.json
 
 #>
 [CmdletBinding()]
