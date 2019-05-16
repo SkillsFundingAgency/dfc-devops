00abbdbe205a819ced0316310fee3265fd478ed8
Richard Lee <ric@dicci.co.uk>
2019-05-16 06:43:45 +0100

    Added Set-SearchDataSources.ps1: datasource creation script

---
+++ PSModules/AzureApiFunctions.psm1
@@ -0,0 +1,60 @@
+function ApiRequest {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string] $Url,
+        [Parameter(Mandatory=$true)]
+        [string] $ApiKey,
+        [Parameter(Mandatory=$false)]
+        [string] $Method = "GET",
+        [Parameter(Mandatory=$false)]
+        [string] $ApiVersion = "2017-11-11",
+        [Parameter(Mandatory=$false)]
+        $Body
+    )
+
+<#
+.SYNOPSIS
+JSON invoke web request for Azure Search
+
+.DESCRIPTION
+Invokes a web request to Azure Search REST API assuming a JSON document
+
+.PARAMETER Url
+Web URL
+
+.PARAMETER ApiKey
+API key to access
+
+.PARAMETER Method
+Optional HTTP method, defaults to GET
+
+.PARAMETER ApiVersion
+Optional, override the API version, defaults to 2017-11-11
+
+.PARAMETER Body
+Optional hash to send as the body
+
+.EXAMPLE
+ApiRequest -SearchName dfc-foo-sch -ResourceGroupName dfc-foo-rg -IndexFilePath C:\path\to\index.json
+#>
+
+    $ApiHeaders = @{
+        "Content-Type" = "application/json"
+        "api-key"      = $ApiKey
+    }
+    $FullUrl = "$($Url)?api-version=$ApiVersion"
+    
+    if ($Body) {
+        $JsonBody = $Body | ConvertTo-Json
+        Write-Verbose "Body - $Body"
+        $WebResponse = Invoke-WebRequest -Uri $FullUrl -Method $Method -Headers $ApiHeaders -Body $JsonBody -UseBasicParsing
+    }
+    else {
+        $WebResponse = Invoke-WebRequest -Uri $FullUrl -Method $Method -Headers $ApiHeaders -UseBasicParsing
+    }
+    
+    if ($WebResponse.StatusCode -eq 200) {
+        return $WebResponse.Content | ConvertFrom-Json
+    }
+}
+
---
+++ PSScripts/Set-SearchDataSources.ps1
@@ -0,0 +1,83 @@
+<#
+.SYNOPSIS
+Creates or modifies Azure Search index
+
+.DESCRIPTION
+Ensures an Azure Search index matches the supplied json document.
+
+.PARAMETER SearchName
+Azure Search name
+
+.PARAMETER ResourceGroupName
+Resource group the Azure Search belongs to
+
+.PARAMETER IndexFilePath
+Full path to the json file including file name
+
+.EXAMPLE
+Set-SearchDataSources -SearchName dfc-foo-sch -ResourceGroupName dfc-foo-rg -IndexFilePath C:\path\to\index.json
+
+#>
+
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory=$true)]
+    [string] $SearchName,
+    [Parameter(Mandatory=$true)]
+    [string] $ResourceGroupName,
+    [Parameter(Mandatory = $true, ParameterSetName = "AsString")]
+    [string]$IndexConfigurationString,
+    [Parameter(Mandatory = $true, ParameterSetName = "AsFilePath")]
+    [string] $IndexFilePath
+)
+
+Import-Module $PSScriptRoot\..\PSModules\AzureApiFunctions
+
+try {
+    if ($PSCmdlet.ParameterSetName -eq "AsFilePath") {
+        if (!(Test-Path $IndexFilePath)) {
+            Write-Error "Configuration File Path can not be found"
+            throw "$_"
+        }
+        $IndexConfiguration = Get-Content $IndexFilePath | ConvertFrom-Json
+    }
+    elseif ($PSCmdlet.ParameterSetName -eq "AsString") {
+        $IndexConfiguration = $IndexConfigurationString | ConvertFrom-Json
+    }
+}
+catch {
+    Write-Error "Config deserialization failed, check JSON is valid"
+    throw "$_"
+}
+
+$SearchParams = @{
+    ResourceType      = "Microsoft.Search/searchServices"
+    ResourceGroupName = $ResourceGroupName
+    ResourceName      = $SearchName
+    ApiVersion        = '2015-08-19'
+}
+$SearchResource = Get-AzureRmResource @SearchParams
+
+$Url = "https://$($SearchResource.name).search.windows.net/datasources"
+
+$SearchParams = @{
+    Action     = 'listAdminKeys'
+    ResourceId = $SearchResource.ResourceId
+    ApiVersion = '2015-08-19'
+    Force      = $true
+}
+$SearchResourceKeys = Invoke-AzureRmResourceAction @SearchParams
+
+foreach ($DataSource in $IndexConfiguration) {
+    try {
+        Write-Verbose "Checking if datasource $($DataSource.name) exists"
+        ApiRequest -Method GET -Url "$Url/$($DataSource.name)" -ApiKey $SearchResourceKeys.PrimaryKey
+    }
+    catch {
+        # index does not exist
+        Write-Host "Creating datasource $($DataSource.name)"
+        ApiRequest -Method POST -Url $Url -ApiKey $SearchResourceKeys.PrimaryKey -Body $DataSource
+        continue
+    }
+}
+    
