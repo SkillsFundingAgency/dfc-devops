a13641163eb0a489b1a8fbcd4521192a4a4c5544
NickGraham101 <nick.graham101@googlemail.com>
2019-05-30 10:49:45 +0100

    Added missing documentation

--- PSScripts/Enable-AzureStorageStaticSite.ps1
+++ PSScripts/Enable-AzureStorageStaticSite.ps1
@@ -1,3 +1,22 @@
+<#
+.SYNOPSIS
+Enables the static website on an Azure Storage Account and tests that the DNS name can be resolved.
+
+.DESCRIPTION
+Enables the static website on an Azure Storage Account and tests that the DNS name can be resolved.
+
+.PARAMETER StorageAccountName
+Name of the storage account that the static site will be enabled on
+
+.PARAMETER IndexDocument
+[Optional] Name of the index file, defaults to index.html
+
+.PARAMETER ErrorDocument404Path
+[Optional] Name of the error page, defaults to error404.html
+
+.EXAMPLE
+Enable-AzureStorageStaticSite.ps1 -StorageAccountName dfcfoosharedstr
+#>
 [CmdletBinding()]
 param(
     [Parameter(Mandatory=$true)]
