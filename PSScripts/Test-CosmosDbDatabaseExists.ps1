b39fe1b658eb2dfb07e76e83df041b7c05d4409f
Mark Godfrey <thelious@gmail.com>
2019-08-12 10:29:28 +0100

    Fixes broken build

--- PSScripts/Test-CosmosDbDatabaseExists.ps1
+++ PSScripts/Test-CosmosDbDatabaseExists.ps1
@@ -1,3 +1,29 @@
+<#
+.SYNOPSIS
+Tests if a CosmosDb database exists.
+
+.DESCRIPTION
+Tests if a CosmosDb  database exists,  outputting the results into an Azure Devops variable.
+
+This is to help work around the ARM templates requiring a certain property at create time, but not when updating.
+
+Requires running Azure  Powershell using the Az cmdlets.
+
+.PARAMETER ResourceGroup
+The resource group the CosmosDb account containing the database resides within
+
+.PARAMETER CosmosDbAccountName
+The CosmosDb account containing the database
+
+.PARAMETER DatabaseName
+The name of the database to check the existance of
+
+.EXAMPLE
+Test-CosmosDbDatabaseExists.ps1 -ResourceGroup SomeResourceGroup -CosmosDbAccountName SomeDatabaseAccount -DatabaseName SomeDatabase
+
+#>
+
+[CmdletBinding()]
 param(
     [Parameter(Mandatory=$true)]
     [string] $ResourceGroup,
@@ -10,4 +36,4 @@ param(
 $allDatabases = Get-AzResource -ResourceGroupName CosmosDbTest -ResourceName "$($CosmosDbAccountName)/sql/" -ResourceType "Microsoft.DocumentDb/databaseAccounts/apis/databases"  -ApiVersion 2016-03-31 -ErrorAction SilentlyContinue
 $selectedDatabase = $allDatabases | Where-Object { $_.Properties.id -eq $DatabaseName }
 
-Write-Host "##vso[task.setvariable variable=CosmosDbDatabaseExists]$($selectedDatabase -ne $null)"
+Write-Host "##vso[task.setvariable variable=CosmosDbDatabaseExists]$($null -ne $selectedDatabase)"
