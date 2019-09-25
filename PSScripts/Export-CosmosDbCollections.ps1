6b33794b128da75576ff889395255a0d9c919b44
Mark Godfrey <thelious@gmail.com>
2019-09-25 15:30:02 +0100

    Adds documentation to satifsy repo code analysis

--- PSScripts/Export-CosmosDbCollections.ps1
+++ PSScripts/Export-CosmosDbCollections.ps1
@@ -1,9 +1,38 @@
 <#
-    .SUMMARY
-    Exports documents from CosmosDb collections to Azure Storage blob.  Assumes that collections and databases are identically named as in DSS project.
 
-    .NOTES
-    Requires https://cosmosdbportalstorage.blob.core.windows.net/datamigrationtool/2018.02.28-1.8.1/dt-1.8.1.zip to be extracted to C:\Program Files (x86)\AzureCosmosDBDataMigrationTool\
+.SYNOPSIS
+Exports documents from CosmosDb collections to Azure Storage blob.  
+
+.DESCRIPTION
+Exports a given number of collections within an Azure CosmosDb database to a storage account.
+
+.PARAMETER CosmosAccountName
+The name of the cosmos account to export from
+
+.PARAMETER Database
+The name of the cosmos database to export from
+
+.PARAMETER Collections
+An array of strings representing the names of the collections to export.
+These are case sensitive.
+
+.PARAMETER CosmosKey
+An access key for the CosmosDb account
+
+.PARAMETER ContainerUrl
+The URL to the blob storage folder to save the exported cosmos collections to
+
+.PARAMETER StorageKey
+An access key for the storage account
+
+.PARAMETER DataMigrationToolLocation
+The full path to the dt.exe binary. Optional!
+
+.NOTES
+Requires https://cosmosdbportalstorage.blob.core.windows.net/datamigrationtool/2018.02.28-1.8.1/dt-1.8.1.zip to be extracted to C:\Program Files (x86)\AzureCosmosDBDataMigrationTool\
+
+.EXAMPLE
+Export-CosmosdbCollections -CosmosDbAccount aCosmosdbAccount -Database aDatabase -Collections @("coll1", "coll2") -CosmosKey aCosmosAccessKey -ContainerUrl https://storageAccount.blob.core.windows.net/backupFolder -StorageKey accessKeyToStorageAccount
 
 #>
 
