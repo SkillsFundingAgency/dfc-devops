2890445eb7b243b86a6812742c37f2264b3a0e21
Richard Lee <ric@dicci.co.uk>
2019-10-03 15:54:22 +0100

    Copy-Database moved in from dfc-digital with Pester tests

---
+++ PSScripts/Copy-Database.ps1
@@ -0,0 +1,144 @@
+<#
+.SYNOPSIS
+Copys current database to new database release version
+
+.DESCRIPTION
+Copys the current database to a new database release version.
+The copied database has -r999 tagged at the end, where 999 is the release number.
+
+.PARAMETER ServerName
+SQL server name (or FQDN).
+If a period is found in the server name it will assume a FQDN.
+
+.PARAMETER DatabaseName
+The database name to use as the source.
+Either this can be specified or an app service name can be specified.
+
+.PARAMETER AppServiceName
+Name of the App Service to get the database name from.
+Either this can be specified or the database name passed in directly.
+
+.PARAMETER DatabaseNameAppSetting
+[Optional] Name of the app setting that contains the database name.
+Defaults to DatabaseVersion if not specified.
+
+.PARAMETER ReleaseNumber
+[Optional] Release number for the new database.
+Defaults to the first part (before the first dash) of environment variable RELEASE_RELEASENAME if not passed.
+Eg. if environment variable RELEASE_RELEASENAME was set to 123-release this would default to 123
+
+.PARAMETER RetentionDays
+[Optional] The Point In Time Retention (PITR) period in days
+Defaults to 35 if not set
+
+.EXAMPLE
+Copy-Database -ServerName dfc-foo-bar-sql -AppServiceName dfc-foo-bar-as
+
+#>
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory = $true)]
+    [String]$ServerName,
+    [Parameter(Mandatory = $true, ParameterSetName = "FromParam")]
+    [String] $DatabaseName,
+    [Parameter(Mandatory = $true, ParameterSetName = "FromAppService")]
+    [String] $AppServiceName,
+    [Parameter(Mandatory = $false, ParameterSetName = "FromAppService")]
+    [String] $DatabaseNameAppSetting = "DatabaseVersion",
+    [Parameter(Mandatory = $false)]
+    [ValidateRange(1, [int]::MaxValue)]
+    [int] $ReleaseNumber,
+    [Parameter(Mandatory = $false)]
+    [ValidateRange(7, 35)]
+    [int] $RetentionDays = 35
+)
+
+try {
+    if ($ServerName.IndexOf(".") -gt 0) {
+        # extract short name from fqdn
+        $ServerName = $ServerName.Substring(0, $ServerName.IndexOf("."))
+    }
+
+    # retrieve server resource
+    Write-Verbose "Searching for server resource $ServerName"
+    $ServerResource = Get-AzResource -Name $ServerName -ResourceType "Microsoft.Sql/servers" -ErrorAction SilentlyContinue
+    if (!$ServerResource) {
+        throw "Could not find server resource $ServerName"
+    }
+    
+    if ($PSBoundParameters.ContainsKey("AppServiceName")) {
+        # get database name from App Service
+
+        # first check App Service exists
+        Write-Verbose "Searching for app service $AppServiceName"
+        $AppServiceResource = Get-AzResource -Name $AppServiceName -ResourceType "Microsoft.Web/sites" -ErrorAction SilentlyContinue
+        if (!$AppServiceResource) {
+            throw "Could not find app service with name $AppServiceName"
+        }
+
+        # then read in app settings from App Service
+        $AppService = Get-AzWebApp -ResourceGroupName $AppServiceResource.ResourceGroupName -Name $AppServiceName
+        $DatabaseName = ($AppService.SiteConfig.AppSettings | Where-Object {$_.Name -eq $DatabaseNameAppSetting}).Value
+        if (!$DatabaseName) {
+            $AppSettingPairs = "App settings"
+            foreach ($val in $AppService.SiteConfig.AppSettings) { $AppSettingPairs += "; $($val.name) = $($val.value)" }
+            throw "Could not determine current database version from $DatabaseNameAppSetting setting. $AppSettingPairs"
+        }
+    }
+
+    # check for source db
+    $SourceDatabase = Get-AzSqlDatabase -ResourceGroupName $ServerResource.ResourceGroupName -ServerName $ServerName -DatabaseName $DatabaseName -ErrorAction SilentlyContinue
+    Write-Verbose "Searching for current database $DatabaseName"
+    if (!$SourceDatabase) {
+        throw "Could not find current database $DatabaseName"
+    }
+
+    # set build number if it is not provided
+    if (!$PSBoundParameters.ContainsKey("ReleaseNumber")) {
+        $ReleaseNumber = $ENV:RELEASE_RELEASENAME.Split("-")[0]
+    }
+
+    # determine if the database name contains a release number (ends in -r999 where 999 is any number)
+    $RegexMatch = [Regex]::Match($DatabaseName, "\-[rR]([0-9]+)$")
+    if ($RegexMatch.Success -eq "True") {
+        # remove the version number leaving just the database name
+        $DatabaseNameGenesis = $DatabaseName.Substring(0, $DatabaseName.LastIndexOf("-"))
+    } else {
+        $DatabaseNameGenesis = $DatabaseName
+    }
+
+    $CopyDatabaseName = "$DatabaseNameGenesis-r$ReleaseNumber"
+
+    # check for existing db with the same name $CopyDatabaseName
+    $ExistingDatabaseCopy = Get-AzSqlDatabase -ResourceGroupName $ServerResource.ResourceGroupName -ServerName $ServerName -DatabaseName $CopyDatabaseName -ErrorAction SilentlyContinue
+    if (!$ExistingDatabaseCopy) {
+        # copy db
+        Write-Output "Copying $DatabaseName to $CopyDatabaseName"
+
+        $DatabaseCopyParameters = @{
+            ResourceGroupName = $ServerResource.ResourceGroupName
+            ServerName        = $ServerName
+            DatabaseName      = $DatabaseName
+            CopyDatabaseName  = $CopyDatabaseName
+        }
+        if ($SourceDatabase.SkuName -eq "ElasticPool") {
+            $DatabaseCopyParameters.ElasticPoolName = $SourceDatabase.ElasticPoolName
+        }
+
+        $StopWatch = [System.Diagnostics.StopWatch]::StartNew()
+        $CopiedDatabase = New-AzSqlDatabaseCopy @DatabaseCopyParameters
+        Set-AzSqlDatabaseBackupShortTermRetentionPolicy -ResourceGroupName $CopiedDatabase.ResourceGroupName -ServerName $CopiedDatabase.ServerName -DatabaseName $CopyDatabaseName -RetentionDays $RetentionDays
+        $ElapsedTime = $StopWatch.Elapsed.ToString('hh\:mm\:ss')
+
+        Write-Output "Database copy completed in $ElapsedTime"   
+    } else {
+        Write-Output "A database copy with name $CopyDatabaseName exists. Skipping"
+    }
+
+    # return values as Azure DevOps variables
+    Write-Output "##vso[task.setvariable variable=CurrentDatabaseName;]$($DatabaseName)"
+    Write-Output "##vso[task.setvariable variable=CopyDatabaseName;]$($CopyDatabaseName)"
+}
+catch {
+    throw $_
+}
---
+++ Tests/U024.PSScripts.Copy-Database.Tests.ps1
@@ -0,0 +1,182 @@
+Push-Location -Path $PSScriptRoot\..\PSScripts\
+
+Describe "Copy-Database" -Tag "Unit" {
+
+    # mocks
+    Mock Get-AzResource -ParameterFilter { $Name -eq "dfc-foo-bar-sql" } -MockWith { return @{
+        Name              = "dfc-foo-bar-sql"
+        ResourceGroupName = "dfc-foo-bar-rg"
+        ResourceType      = "Microsoft.Sql/servers"
+    }}
+    Mock Get-AzResource -ParameterFilter { $Name -eq "dfc-foo-bar-as" } -MockWith { return @{
+        Name              = "dfc-foo-bar-as"
+        ResourceGroupName = "dfc-foo-bar-rg"
+        ResourceType      = "Microsoft.Web/sites"
+    }}
+    Mock Get-AzWebApp -MockWith { return @{
+        SiteConfig = @{
+            AppSettings = @(
+                @{ Name = "DatabaseName"; Value ="dfc-foo-db"},
+                @{ Name = "DatabaseVersion"; Value ="dfc-bar-db-r101"}
+    )}}}
+    Mock Get-AzSqlDatabase -ParameterFilter { $DatabaseName.EndsWith('-r123') } -MockWith { return $null }
+    Mock Get-AzSqlDatabase -MockWith { return  @{
+        ServerName        = "dfc-foo-bar-sql"
+        ResourceGroupName = "dfc-foo-bar-rg"
+        DatabaseName      = "dfc-foo-bar-db" 
+    }}
+    Mock New-AzSqlDatabaseCopy -MockWith { return  @{
+        ServerName        = "dfc-foo-bar-sql"
+        ResourceGroupName = "dfc-foo-bar-rg"
+        DatabaseName      = "dfc-foo-bar-db" 
+    }}
+    Mock Set-AzSqlDatabaseBackupShortTermRetentionPolicy
+
+    $MockParameters = @{
+        ServerName     = "dfc-foo-bar-sql.database.windows.net"
+        AppServiceName = "dfc-foo-bar-as"
+        ReleaseNumber  = 123
+    }
+
+    Context "When the Azure SQL Server does not exist" {
+        Mock Get-AzResource -ParameterFilter { $Name -eq "dfc-foo-bar-sql" } -MockWith { return $null }
+
+        It "should throw an error" {
+            { .\Copy-Database @MockParameters } | Should -Throw
+        }
+
+        It "should attempt to get the Azure SQL Server resourse (strip the FQDN)" {
+            Assert-MockCalled Get-AzResource -ParameterFilter { $Name -eq "dfc-foo-bar-sql" } -Exactly 1
+        }
+
+        It "should not have called any other Azure cmdlet" {
+            Assert-MockCalled Get-AzWebApp -Exactly 0
+            Assert-MockCalled Get-AzSqlDatabase -Exactly 0
+            Assert-MockCalled New-AzSqlDatabaseCopy -Exactly 0
+            Assert-MockCalled Set-AzSqlDatabaseBackupShortTermRetentionPolicy -Exactly 0
+        }
+    }
+
+    Context "When the App Service does not exist" {
+        Mock Get-AzResource -ParameterFilter { $Name -eq "dfc-foo-bar-as" } -MockWith { return $null }
+
+        It "Should throw an error" {
+            { .\Copy-Database @MockParameters } | Should -Throw
+        }
+
+        It "should attempt to get the App Service resourse" {
+            Assert-MockCalled Get-AzResource -ParameterFilter { $Name -eq "dfc-foo-bar-as" } -Exactly 1
+        }
+
+        It "should not have called any other Azure cmdlet" {
+            Assert-MockCalled Get-AzWebApp -Exactly 0
+            Assert-MockCalled Get-AzSqlDatabase -Exactly 0
+            Assert-MockCalled New-AzSqlDatabaseCopy -Exactly 0
+            Assert-MockCalled Set-AzSqlDatabaseBackupShortTermRetentionPolicy -Exactly 0
+        }
+    }
+
+    Context "When the source database does not exist" {
+        Mock Get-AzSqlDatabase -MockWith { return $null }
+
+        It "Should throw an error" {
+            { .\Copy-Database @MockParameters } | Should -Throw
+        }
+    }
+
+    Context "When copy database as specified by app service" {
+
+        $output = .\Copy-Database @MockParameters
+
+
+        It "should attempt to get info on both Azure SQL Server and App Service resourses" {
+            Assert-MockCalled Get-AzResource -Exactly 2
+        }
+
+        It "should get the App Service details" {
+            Assert-MockCalled Get-AzWebApp -Exactly 1
+        }
+
+        It "should check for both source and destination databases" {
+            Assert-MockCalled Get-AzSqlDatabase -Exactly 2
+        }
+
+        It "should copy the database dfc-bar-db-r101 to dfc-bar-db-r123" {
+            Assert-MockCalled New-AzSqlDatabaseCopy #-ParameterFilter { $DatabaseName -eq "dfc-bar-db-r101" -and $CopyDatabaseName -eq "dfc-bar-db-r123" } -Exactly 1
+        }
+
+        It "should set the short term retention period to 35 days (default)" {
+            Assert-MockCalled Set-AzSqlDatabaseBackupShortTermRetentionPolicy #-ParameterFilter { $RetentionDays -eq 35 } -Exactly 1
+        }
+
+        It "output should contain copy message" {
+            $output | Should contain "Copying dfc-bar-db-r101 to dfc-bar-db-r123"
+        }    
+    }
+
+    Context "When copy database with the defaults changed" {
+        $MockParameters.DatabaseNameAppSetting = "DatabaseName"
+        $MockParameters.RetentionDays = 28
+
+        $output = .\Copy-Database @MockParameters
+
+
+        It "should attempt to get info on both Azure SQL Server and App Service resourses" {
+            Assert-MockCalled Get-AzResource -Exactly 2
+        }
+
+        It "should get the App Service details" {
+            Assert-MockCalled Get-AzWebApp -Exactly 1
+        }
+
+        It "should check for both source and destination databases" {
+            Assert-MockCalled Get-AzSqlDatabase -Exactly 2
+        }
+
+        It "should copy the database dfc-foo-db (from DatabaseName app setting) to dfc-foo-db-r123" {
+            Assert-MockCalled New-AzSqlDatabaseCopy -ParameterFilter { $DatabaseName -eq "dfc-foo-db" -and $CopyDatabaseName -eq "dfc-foo-db-r123" } -Exactly 1
+        }
+
+        It "should set the short term retention period to 28 days (value passed in)" {
+            Assert-MockCalled Set-AzSqlDatabaseBackupShortTermRetentionPolicy -ParameterFilter { $RetentionDays -eq 28 } -Exactly 1
+        }
+
+        It "output should contain copy message" {
+            $output | Should contain "Copying dfc-foo-db to dfc-foo-db-r123"
+        }
+    }
+
+    Context "When the copy database already exists" {
+        $MockParameters.ReleaseNumber = 101
+
+        $output = .\Copy-Database @MockParameters
+
+
+        It "should attempt to get info on both Azure SQL Server and App Service resourses" {
+            Assert-MockCalled Get-AzResource -Exactly 2
+        }
+
+        It "should get the App Service details" {
+            Assert-MockCalled Get-AzWebApp -Exactly 1
+        }
+
+        It "should check for both source and destination databases" {
+            Assert-MockCalled Get-AzSqlDatabase -Exactly 2
+        }
+
+        It "should not copy the database" {
+            Assert-MockCalled New-AzSqlDatabaseCopy -Exactly 0
+        }
+
+        It "should not set the short term retention period" {
+            Assert-MockCalled Set-AzSqlDatabaseBackupShortTermRetentionPolicy -Exactly 0
+        }
+
+        It "output should contain skipping message" {
+            $output | Should contain "A database copy with name dfc-foo-db-r101 exists. Skipping"
+        }
+    }
+
+}
+
+Push-Location -Path $PSScriptRoot
