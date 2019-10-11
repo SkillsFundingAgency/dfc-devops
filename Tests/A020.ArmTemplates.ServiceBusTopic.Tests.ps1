cd03670ad4a47440a4a5c6f7197f2d20c26974bf
Richard Lee <ric@dicci.co.uk>
2019-10-11 13:16:16 +0100

    Service Bus topic and topic subscription templates added

---
+++ Tests/A020.ArmTemplates.ServiceBusTopic.Tests.ps1
@@ -0,0 +1,29 @@
+# common variables
+$ResourceGroupName = "dfc-test-template-rg"
+$TemplateFile = "$PSScriptRoot\..\ArmTemplates\ServiceBus\servicebus-topic.json"
+
+Describe "Service Bus Topic Deployment Tests" -Tag "Acceptance" {
+  
+  Context "When an app gateway is deployed with just a single pool" {
+    $TemplateParameters = @{
+      servicebusName      = "dfc-foo-bar-ns"
+      serviceBusTopicName = "topic-name"
+    }
+    $TestTemplateParams = @{
+      ResourceGroupName       = $ResourceGroupName
+      TemplateFile            = $TemplateFile
+      TemplateParameterObject = $TemplateParameters
+    }
+
+    $output = Test-AzureRmResourceGroupDeployment @TestTemplateParams
+  
+    It "Should be deployed successfully" {
+      $output | Should -Be $null
+    }
+
+    if ($output) {
+      Write-Error $output.Message
+    }
+
+  }
+}
