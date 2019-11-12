ab2b6cc53e8e55ec87f7d6c054a6b9f56deb2b32
Mark Godfrey <thelious@gmail.com>
2019-11-12 09:28:14 +0000

    NCSD-2115: Creates a function app naming and api versioning script resembling that in dss-devops

---
+++ Tests/U025.PSScripts.Get-VersionedFunctionAppName.ps1
@@ -0,0 +1,70 @@
+Describe "Get-VersionedFunctionAppName" -Tag "Unit" {
+
+    Context "When getting a versioned function app name with pooly formatted function app" {
+
+        It "should throw an exception" {
+            $env:Build_SourceBranchName = "dev"
+            $env:FunctionAppBaseName = "someFunctionApp"
+
+            {
+                & $PSScriptRoot/../PSScripts/Get-VersionedFunctionAppName
+            } | Should Throw
+        }
+    }
+
+    Context "When getting a versioned app" {
+        $testData = @(
+            @{ SourceBranch = "dev"; FunctionBaseName = "dfc-dev-test-fa"; ExpectedResult = "dfc-dev-test-v1-fa" }
+            @{ SourceBranch = "master"; FunctionBaseName = "dfc-dev-test-fa"; ExpectedResult = "dfc-dev-test-v1-fa" }
+            @{ SourceBranch = "v1-dev"; FunctionBaseName = "dfc-dev-test-fa"; ExpectedResult = "dfc-dev-test-v1-fa" }
+            @{ SourceBranch = "v1-master"; FunctionBaseName = "dfc-dev-test-fa"; ExpectedResult = "dfc-dev-test-v1-fa" }
+            @{ SourceBranch = "dev-v1"; FunctionBaseName = "dfc-dev-test-fa"; ExpectedResult = "dfc-dev-test-v1-fa" }
+            @{ SourceBranch = "master-v1"; FunctionBaseName = "dfc-dev-test-fa"; ExpectedResult = "dfc-dev-test-v1-fa" }
+            @{ SourceBranch = "v999999-dev"; FunctionBaseName = "dfc-dev-test-fa"; ExpectedResult = "dfc-dev-test-v999999-fa" }
+            @{ SourceBranch = "v999999-master"; FunctionBaseName = "dfc-dev-test-fa"; ExpectedResult = "dfc-dev-test-v999999-fa" }
+            @{ SourceBranch = "dev-v999999"; FunctionBaseName = "dfc-dev-test-fa"; ExpectedResult = "dfc-dev-test-v999999-fa" }
+            @{ SourceBranch = "master-v999999"; FunctionBaseName = "dfc-dev-test-fa"; ExpectedResult = "dfc-dev-test-v999999-fa" }
+        )
+
+        It "Should return '<ExpectedResult>' for branch <SourceBranch>' and functionName '<FunctionBaseName>'" -TestCases $testData {
+            param($SourceBranch, $FunctionBaseName, $ExpectedResult)
+
+            $env:Build_SourceBranchName = $SourceBranch
+            $env:FunctionAppBaseName = $FunctionBaseName
+
+            $allOutput = & $PSScriptRoot/../PSScripts/Get-VersionedFunctionAppName
+
+            $functionAppOutput = $allOutput | Where-Object { $_ -like "*FunctionAppName*" }
+
+            $functionAppOutput | Should Be "##vso[task.setvariable variable=FunctionAppName;isOutput=false]$ExpectedResult"
+        }
+    }
+
+    Context "When getting an api version" {
+        $testData = @(
+            @{ SourceBranch = "dev"; FunctionBaseName = "dfc-dev-test-fa"; ExpectedResult = "v1" }
+            @{ SourceBranch = "master"; FunctionBaseName = "dfc-dev-test-fa"; ExpectedResult = "v1" }
+            @{ SourceBranch = "v1-dev"; FunctionBaseName = "dfc-dev-test-fa"; ExpectedResult = "v1" }
+            @{ SourceBranch = "v1-master"; FunctionBaseName = "dfc-dev-test-fa"; ExpectedResult = "v1" }
+            @{ SourceBranch = "dev-v1"; FunctionBaseName = "dfc-dev-test-fa"; ExpectedResult = "v1" }
+            @{ SourceBranch = "master-v1"; FunctionBaseName = "dfc-dev-test-fa"; ExpectedResult = "v1" }
+            @{ SourceBranch = "v999999-dev"; FunctionBaseName = "dfc-dev-test-fa"; ExpectedResult = "v999999" }
+            @{ SourceBranch = "v999999-master"; FunctionBaseName = "dfc-dev-test-fa"; ExpectedResult = "v999999" }
+            @{ SourceBranch = "dev-v999999"; FunctionBaseName = "dfc-dev-test-fa"; ExpectedResult = "v999999" }
+            @{ SourceBranch = "master-v999999"; FunctionBaseName = "dfc-dev-test-fa"; ExpectedResult = "v999999" }
+        )
+
+        It "Should return '<ExpectedResult>' for branch <SourceBranch>' and functionName '<FunctionBaseName>'" -TestCases $testData {
+            param($SourceBranch, $FunctionBaseName, $ExpectedResult)
+
+            $env:Build_SourceBranchName = $SourceBranch
+            $env:FunctionAppBaseName = $FunctionBaseName
+
+            $allOutput = & $PSScriptRoot/../PSScripts/Get-VersionedFunctionAppName
+
+            $functionAppOutput = $allOutput | Where-Object { $_ -like "*ApiVersion*" }
+
+            $functionAppOutput | Should Be "##vso[task.setvariable variable=ApiVersion;isOutput=false]$ExpectedResult"
+        }
+    }
+}
