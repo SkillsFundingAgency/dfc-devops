7e4f695f04d02539ac22c772bee1d0f4b44d6859
Mark Godfrey <36482610+thelious1974@users.noreply.github.com>
2020-01-13 09:38:51 +0000

    NCSD-2515: Adds script to create tables in storage accounts (#194)
    * NCSD-2515: Adds script to create tables in storage accounts
    
    * Update doc string
    
    * Various updates from review

---
+++ PSScripts/New-TableOnStorageAccount.ps1
@@ -0,0 +1,43 @@
+<#
+.SYNOPSIS
+Creates a table on a storage account if it doesn't already exist
+
+.DESCRIPTION
+Creates a table on a storage account if it doesn't already exist
+
+.PARAMETER StorageAccountName
+The Storage Account to create the table upon
+
+.PARAMETER StorageAccountKey
+The key to the storage account specified in the StorageAccountName parameter
+
+.PARAMETER TableName
+The name of the table to create
+
+.EXAMPLE
+New-TableOnStorageAccount.ps1 -StorageAccountName dfcfoostr -StorageAccountKey not-a-real-key -TableName aTableToCreate
+
+#>
+
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory=$true)]
+    [string] $StorageAccountName,
+    [Parameter(Mandatory=$true)]
+    [string] $StorageAccountKey,
+    [Parameter(Mandatory=$true)]
+    [string] $TableName
+)
+
+Write-Verbose "Creating storage context"
+$context = New-AzStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $StorageAccountKey
+
+Write-Verbose "Attempting to fetch table '$($TableName)'"
+$table = Get-AzStorageTable -Name $TableName -Context $context -ErrorAction SilentlyContinue
+
+if(!$table) {
+    Write-Verbose "Table '$($TableName)' does not exist, creating"
+    New-AzStorageTable -Context $context -Name $TableName
+} else {
+    Write-Verbose "Table '$($TableName)' already exists, skipping creation"
+}
