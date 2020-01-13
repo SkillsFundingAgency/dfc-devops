49c7679327dfee8f3b5a6a4d4d115ec3a44c54a9
Mark Godfrey <thelious@gmail.com>
2020-01-13 14:10:36 +0000

    NCSD-2515: fetch storage account secret inside the script

--- PSScripts/New-TableOnStorageAccount.ps1
+++ PSScripts/New-TableOnStorageAccount.ps1
@@ -5,11 +5,11 @@ Creates a table on a storage account if it doesn't already exist
 .DESCRIPTION
 Creates a table on a storage account if it doesn't already exist
 
+.PARAMETER ResourceGroupName
+The name of the resource group containing the storage account
+
 .PARAMETER StorageAccountName
 The Storage Account to create the table upon
-
-.PARAMETER StorageAccountKey
-The key to the storage account specified in the StorageAccountName parameter
 
 .PARAMETER TableName
 The name of the table to create
@@ -22,15 +22,24 @@ New-TableOnStorageAccount.ps1 -StorageAccountName dfcfoostr -StorageAccountKey n
 [CmdletBinding()]
 param(
     [Parameter(Mandatory=$true)]
-    [string] $StorageAccountName,
+    [string] $ResourceGroupName,
     [Parameter(Mandatory=$true)]
-    [string] $StorageAccountKey,
+    [string] $StorageAccountName,
     [Parameter(Mandatory=$true)]
     [string] $TableName
 )
 
+# Fetch storage account key via Az module
+Write-Verbose "Fetching storage account keys"
+$storageAccountKeys = Get-AzStorageAccountKey -ResourceGroupName $ResourceGroupName -Name $StorageAccountName -ErrorAction SilentlyContinue
+
+if(!$storageAccountKeys) {
+    throw "Unable to fetch account keys from storage account '$($StorageAccountName)'"
+}
+$accountKey = ($storageAccountKeys | Where-Object { $_.keyName -eq "key1" }).Value
+
 Write-Verbose "Creating storage context"
-$context = New-AzStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $StorageAccountKey
+$context = New-AzStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $accountKey
 
 Write-Verbose "Attempting to fetch table '$($TableName)'"
 $table = Get-AzStorageTable -Name $TableName -Context $context -ErrorAction SilentlyContinue
--- Tests/U029-New-TableOnStorageAccount.ps1
+++ Tests/U029-New-TableOnStorageAccount.ps1
@@ -10,6 +10,11 @@ Describe "New-TableOnStorageAccount unit tests" -Tag "Unit" {
         param($StorageAccountName, $StorageAccountKey)
     }
 
+    function Get-AzStorageAccountKey {
+        [CmdletBinding()]
+        param($ResourceGroupName, $Name)
+    }
+
     function Get-AzStorageTable {
         [CmdletBinding()]
         param($Context, $Name)
@@ -23,21 +28,64 @@ Describe "New-TableOnStorageAccount unit tests" -Tag "Unit" {
     Mock New-AzStorageContext
     Mock New-AzStorageTable
     Mock Get-AzStorageTable -MockWith { return $null }
+    Mock Get-AzStorageAccountKey -MockWith { 
+        return @{
+            keyName = "key1"
+            Value = "not-a-real-key"
+        }
+    }
+
+    Context "When the storage account does not exist" {
+        Mock Get-AzStorageAccountKey -MockWith { return $null }
+
+        It "should throw an exception" {
+            { 
+                ./New-TableOnStorageAccount -StorageAccountName SomeStorageAccount -ResourceGroupName aResourceGroup -TableName SomeTable
+            } | Should throw "Unable to fetch account keys from storage account 'SomeStorageAccount'"
+        }
+
+        It "should get storage account keys" {
+            Assert-MockCalled Get-AzStorageAccountKey -Exactly 1 -ParameterFilter {
+                $ResourceGroupName -eq "aResourceGroup" -and `
+                $Name -eq "SomeStorageAccount"
+            }
+        }
+
+        It "should not create any storage contexts" {
+            Assert-MockCalled New-AzStorageContext -Exactly 0
+        }
+
+        It "should not get any storage tables" {
+            Assert-MockCalled Get-AzStorageTable -Exactly 0
+        }
+
+        It "should not create any tables" {
+            Assert-MockCalled New-AzStorageTable -Exactly 0
+        }
+    }
 
     Context "When the table exists" {
-    
-        Mock Get-AzStorageTable -MockWith { 
+
+        Mock Get-AzStorageTable -MockWith {
             return @{
                 Uri = "https://some/uri"
             }
         }
 
-        ./New-TableOnStorageAccount -StorageAccountName SomeStorageAccount -StorageAccountKey aStorageAccountKey -TableName SomeTable
+        ./New-TableOnStorageAccount -StorageAccountName SomeStorageAccount -ResourceGroupName aResourceGroup -TableName SomeTable
+
+
+        It "should get storage account keys" {
+            Assert-MockCalled Get-AzStorageAccountKey -Exactly 1 -ParameterFilter {
+                $ResourceGroupName -eq "aResourceGroup" -and `
+                $Name -eq "SomeStorageAccount"
+            }
+        }
 
         It "Should create a new storage context"  {
-            Assert-MockCalled New-AzStorageContext -Exactly 1 -ParameterFilter { 
+            Assert-MockCalled New-AzStorageContext -Exactly 1 -ParameterFilter {
                 $StorageAccountName -eq "SomeStorageAccount" -and `
-                $StorageAccountKey -eq "aStorageAccountKey"
+                $StorageAccountKey -eq "not-a-real-key"
             }
         }
         
@@ -52,12 +100,20 @@ Describe "New-TableOnStorageAccount unit tests" -Tag "Unit" {
 
     Context "When the table does not exist" {
 
-        ./New-TableOnStorageAccount -StorageAccountName SomeStorageAccount -StorageAccountKey aStorageAccountKey -TableName SomeTable
+        ./New-TableOnStorageAccount -StorageAccountName SomeStorageAccount -ResourceGroupName aResourceGroup -TableName SomeTable
+
+
+        It "should get storage account keys" {
+            Assert-MockCalled Get-AzStorageAccountKey -Exactly 1 -ParameterFilter {
+                $ResourceGroupName -eq "aResourceGroup" -and `
+                $Name -eq "SomeStorageAccount"
+            }
+        }
 
         It "Should create a new storage context"  {
-            Assert-MockCalled New-AzStorageContext -Exactly 1 -ParameterFilter { 
+            Assert-MockCalled New-AzStorageContext -Exactly 1 -ParameterFilter {
                 $StorageAccountName -eq "SomeStorageAccount" -and `
-                $StorageAccountKey -eq "aStorageAccountKey"
+                $StorageAccountKey -eq "not-a-real-key"
             }
         }
         
