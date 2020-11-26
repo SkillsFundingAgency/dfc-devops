701ad4eb8dd8c237cb4093d660277febe845956a
Lee Frampton <lee.frampton@nimbusdti.co.uk>
2020-11-26 14:36:41 +0000

    added New-EventGridSubscription.ps1

---
+++ PSCoreScripts/New-EventGridSubscription.ps1
@@ -0,0 +1,39 @@
+<#
+.SYNOPSIS
+Creates EventGrid Subscription.  
+
+.DESCRIPTION
+Creates EventGrid Subscription.  The topic and function app must already exist along with their resourcegroups
+
+.PARAMETER EventGridSubscriptionName
+The EventGrid Subscription Name
+
+.PARAMETER TopicResourceGroup
+The Topic ResourceGroup
+
+.PARAMETER Topic
+The name of the Topic
+
+.PARAMETER SubscriptionEndPoint
+The name of theSubscription EndPoint
+#>
+
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory=$true)]
+    [String]$EventGridSubscriptionName,
+    [Parameter(Mandatory=$true)]
+    [String]$TopicResourceGroup,
+    [Parameter(Mandatory=$true)]
+    [String]$Topic,
+    [Parameter(Mandatory=$true)]
+    [String]$SubscriptionEndPoint
+)
+
+New-AzEventGridSubscription `
+    -EventSubscriptionName  $EventGridSubscriptionName `
+    -ResourceGroupName $TopicResourceGroup `
+    -TopicName $Topic `
+    -EndpointType 'webhook' `
+    -Endpoint  $SubscriptionEndPoint `
+    -Verbose
