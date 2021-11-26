c87244e31e9be8f4520c94b7471275c90b178907
olusola-adio <olusola_adio@hotmail.com>
2021-11-26 03:54:53 +0000

    NCSD-5986-failed-tests

--- Tests/Invoke-QualityTests.ps1
+++ Tests/Invoke-QualityTests.ps1
@@ -16,55 +16,62 @@ Invoke-QualityTests.ps1
 [CmdletBinding()]
 param()
 
-$pesterModule = Get-Module -Name Pester -ListAvailable | Where-Object {$_.Version -like '5.*'}
-if (!$pesterModule) {
-    try {
-        Write-Host "Installing Pester"
-        Install-Module -Name Pester -Force -SkipPublisherCheck -MinimumVersion "5.0.0"
-        Write-Host "Getting Pester version"
-        $pesterModule = Get-Module -Name Pester -ListAvailable | Where-Object {$_.Version -like '5.*'}
+try {
+    $pesterModule = Get-Module -Name Pester -ListAvailable | Where-Object { $_.Version -like '5.*' }
+    if (!$pesterModule) {
+        try {
+            Write-Host "Installing Pester"
+            Install-Module -Name Pester -Force -SkipPublisherCheck -MinimumVersion "5.0.0"
+            Write-Host "Getting Pester version"
+            $pesterModule = Get-Module -Name Pester -ListAvailable | Where-Object { $_.Version -like '5.*' }
+        }
+        catch {
+            Write-Error "Failed to install the Pester module."
+        }
     }
-    catch {
-        Write-Error "Failed to install the Pester module."
+
+    $pesterModule | Import-Module
+
+
+    # Write-Host "Fetching tests:"
+    $Tests = (Get-ChildItem -Path "$($PSScriptRoot)\Quality" -Recurse | Where-Object { $_.Name -like "*.Tests.ps1" }).FullName
+
+    $Params = [ordered]@{
+        Path = $Tests;
+    }
+
+    $Container = New-PesterContainer @Params
+
+    $Configuration = [PesterConfiguration]@{
+        Run        = @{
+            Container = $Container
+        }
+        Output     = @{
+            Verbosity = 'Diagnostic'
+        }
+        Filter     = @{
+            Tag = 'Quality'
+        }
+        TestResult = @{
+            Enabled      = $true
+            OutputFormat = "NUnitXml"
+            OutputPath   = "$PSScriptRoot\TEST-Quality.xml"
+        }
+        Should     = @{
+            ErrorAction = 'Continue'
+        }
+    }
+
+    # Invoke tests
+    $Result = Invoke-Pester -Configuration $Configuration -PassThru
+
+    # report failures
+    if ($Result.FailedCount -gt 0) { 
+        throw "{0} tests did not pass" -f $Result.FailedCount
     }
 }
-
-$pesterModule | Import-Module
-
-
-# Write-Host "Fetching tests:"
-$Tests = (Get-ChildItem -Path "$($PSScriptRoot)\Quality" -Recurse | Where-Object {$_.Name -like "*.Tests.ps1"}).FullName
-
-$Params = [ordered]@{
-    Path = $Tests;
-}
-
-$Container = New-PesterContainer @Params
-
-$Configuration = [PesterConfiguration]@{
-    Run          = @{
-        Container = $Container
-    }
-    Output       = @{
-        Verbosity = 'Diagnostic'
-    }
-    Filter = @{
-        Tag = 'Quality'
-    }
-    TestResult   = @{
-        Enabled      = $true
-        OutputFormat = "NUnitXml"
-        OutputPath   = "$PSScriptRoot\TEST-Quality.xml"
-    }
-    Should = @{
-        ErrorAction = 'Continue'
-    }
-}
-
-# Invoke tests
-$Result = Invoke-Pester -Configuration $Configuration -PassThru
-
-# report failures
-if ($Result.FailedCount -gt 0) { 
-    throw "{0} tests did not pass" -f $Result.FailedCount
+catch {
+    $msg = $_
+    Write-Error -ErrorRecord $msg
+    exit 1
 }
