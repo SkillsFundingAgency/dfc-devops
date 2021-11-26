1cfeb7d9515b253259614841c161e5ede109566c
olusola-adio <olusola_adio@hotmail.com>
2021-11-26 07:30:30 +0000

    NCSD-5986-acceptance-tests

--- Tests/Invoke-AcceptanceTests.ps1
+++ Tests/Invoke-AcceptanceTests.ps1
@@ -1,33 +1,77 @@
 <#
 .SYNOPSIS
-Test runner for ARM acceptance tests
+Test runner for code quality tests
 
 .DESCRIPTION
-Test runner for ARM acceptance tests
+Test runner for code quality tests
 
 .EXAMPLE
-Invoke-AcceptanceTests.ps1
+Invoke-QualityTests.ps1
 
 .EXAMPLE
-Invoke-AcceptanceTests.ps1
-
+Invoke-QualityTests.ps1
 
 #>
 
 [CmdletBinding()]
+param()
 
-$TestParameters = @{
-    OutputFormat = 'NUnitXml'
-    OutputFile   = "$PSScriptRoot\TEST-Acceptance.xml"
-    Script       = "$PSScriptRoot\arm"
-    PassThru     = $True
-    Tag          = "Acceptance"
+try {
+    $pesterModule = Get-Module -Name Pester -ListAvailable | Where-Object { $_.Version -like '5.*' }
+    if (!$pesterModule) {
+        try {
+            Write-Host "Installing Pester"
+            Install-Module -Name Pester -Force -SkipPublisherCheck -MinimumVersion "5.0.0"
+            Write-Host "Getting Pester version"
+            $pesterModule = Get-Module -Name Pester -ListAvailable | Where-Object { $_.Version -like '5.*' }
+        }
+        catch {
+            Write-Error "Failed to install the Pester module."
+        }
+    }
+
+    $pesterModule | Import-Module
+
+
+    # Write-Host "Fetching tests:"
+    $Tests = (Get-ChildItem -Path "$($PSScriptRoot)\arm" -Recurse | Where-Object { $_.Name -like "*.Tests.ps1" }).FullName
+
+    $Params = [ordered]@{
+        Path = $Tests;
+    }
+
+    $Container = New-PesterContainer @Params
+
+    $Configuration = [PesterConfiguration]@{
+        Run        = @{
+            Container = $Container
+        }
+        Output     = @{
+            Verbosity = 'Diagnostic'
+        }
+        Filter     = @{
+            Tag = 'Acceptance'
+        }
+        TestResult = @{
+            Enabled      = $true
+            OutputFormat = "NUnitXml"
+            OutputPath   = "$PSScriptRoot\TEST-Acceptance.xml"
+        }
+        Should     = @{
+            ErrorAction = 'Continue'
+        }
+    }
+
+    # Invoke tests
+    $Result = Invoke-Pester -Configuration $Configuration
+
+    # report failures
+    if ($Result.FailedCount -gt 0) { 
+        throw "{0} tests did not pass" -f $Result.FailedCount
+    }
 }
-
-# Invoke tests
-$Result = Invoke-Pester @TestParameters
-
-# report failures
-if ($Result.FailedCount -ne 0) { 
-    Write-Error "Pester returned $($result.FailedCount) errors"
+catch {
+    $msg = $_
+    Write-Error -ErrorRecord $msg
+    exit 1
 }
