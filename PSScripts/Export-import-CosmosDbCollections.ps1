a20d3f75dbddd19eff1ed75c4977dc78cd6e8ec8
Raph-zek <97975567+Raph-zek@users.noreply.github.com>
2022-05-19 13:09:12 +0100

    Add files via upload
    This to try and export cosmos collections straight from Prod DB into a target environment

---
+++ PSScripts/Export-import-CosmosDbCollections.ps1
@@ -0,0 +1,33 @@
+﻿[CmdletBinding()]
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$CosmosAccountName,
+    [Parameter(Mandatory=$false)]
+    [string]$Database,
+    [Parameter(Mandatory=$false)]
+    [array]$Collections,
+    [Parameter(Mandatory=$true)]
+    [string]$CosmosKey,
+    [Parameter(Mandatory=$true)]
+    ##[string]$StorageKey,
+    ##[Parameter(Mandatory=$false)]
+    [string]$CosmosAccountNameTarget,
+    [Parameter(Mandatory=$false)]
+    [string]$DatabaseTarget,
+    [Parameter(Mandatory=$false)]
+    [string]$CosmosKeyTarget,
+    [Parameter(Mandatory=$true)]
+    [string]$DataMigrationToolLocation = 'C:\Program Files (x86)\AzureCosmosDBDataMigrationTool\dt.exe'
+)
+
+foreach ($collection in $Collections) {
+    Write-Verbose -Message "Backing up collection $collection"
+
+  ##  $parameters = "/s:DocumentDB /s.ConnectionString:AccountEndpoint=https://$CosmosAccountName.documents.azure.com:443/;AccountKey=$CosmosKey;Database=$Database /s.Collection:$Collection /t:JsonFile /t.File:blobs://$StorageKey@$($ContainerUrl.Replace('https://', ''))/$([DateTime]::Now.ToString("yyyy-MM-dd_HHmm"))-$Collection-backup.json"
+    $parameters = "/s:DocumentDB /s.ConnectionString:AccountEndpoint=https://$CosmosAccountName.documents.azure.com:443/;AccountKey=$CosmosKey;Database=$Database /s.Collection:$Collection /t:DocumentDBTarget /t.ConnectionString:AccountEndpoint=https://$CosmosAccountNameTarget.documents.azure.com:443/;AccountKey=$CosmosKeyTarget;Database=$DatabaseTarget /t.Collection:$Collection"
+
+    Write-Verbose -Message "Parameters: $parameters"
+    $cmd = $DataMigrationToolLocation
+    $params = $parameters.Split(" ")
+    & $cmd $params
+}
